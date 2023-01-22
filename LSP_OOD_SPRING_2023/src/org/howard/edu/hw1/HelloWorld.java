623b28e88b6458a7e6126225fc79b50f19f85b8e
ronald <ronald@rons-air.fios-router.home>
2023-01-22 11:26:01 -0500

    This is my fist commit to Github.

---
+++ LSP_OOD_SPRING_2023/src/org/howard/edu/hw1/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+*  Name: Ronald Langrin
+*/
+
+
+package org.howard.edu.hw1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+
+	}
+
+}
